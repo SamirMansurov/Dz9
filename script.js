ed8d1dd0ad0565691010ce630c8d981fe9b0f79f
Samir <samirmansuriv078@gmail.com>
2023-12-19 16:44:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// №1
+const colors = ['red', 'green', 'blue', 'yellow']
+for (let total = 0; total <= colors.length ; total++ ) {
+    console.log(colors[total]);
+}
+
+// №3
+// for(let arr = 0; arr < 20; arr++) {
+//     if(arr % 2 !==0)
+//     console.log(arr);
+// }
+
+
+
+
+
